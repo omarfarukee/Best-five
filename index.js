3f02623b0d0f5537dbe55882959c6e26eefa1049
omarfarukee <umarfaruque1920@gmail.com>
2022-08-23 13:20:05 +0600

    grand final  commit

--- index.js
+++ index.js
@@ -1,123 +1,43 @@
 
 const arrayList = [];
 const arrayListString = parseFloat(arrayList)
- 
-console.log(arrayList)
-document.getElementById('selected1').addEventListener('click', function(){
-
-         const selectMsBtn = document.getElementById('messi');
-         const selectedMsInnerText = selectMsBtn.innerText
+// ------------------<<<<<<<Common function For Selection button >>>>>>>>>>>----------------------------------
+function getYourBest(input, element) {
+    const selectMsBtn = document.getElementById(input);
+    const selectedMsInnerText = selectMsBtn.innerText
         
-         const disableBtn = document.getElementById('selected1')
+         const disableBtn = document.getElementById(element)
          const container=  document.getElementById("player-Container");
                  newElements = document.createElement('li')
                  newElements.innerText = selectedMsInnerText
-                 container.appendChild(newElements)
-
                 if (arrayList.length < 5) {
                     arrayList.push(container)
                     disableBtn.disabled = true;
                     disableBtn.style.backgroundColor = "gray"
+                    container.appendChild(newElements)
                   }
                 else{
                     alert ('You can chose only five')
                 }
+}
+
+document.getElementById('selected1').addEventListener('click',function(){
+    getYourBest('messi', 'selected1')
 })
-document.getElementById('selected2').addEventListener('click', function(){
-
-         const selectNyBtn = document.getElementById('neymar');
-         const selectedNyInnerText = selectNyBtn.innerText
-
-         const disableBtn = document.getElementById('selected2')
-         const container=  document.getElementById("player-Container");
-                 newElements = document.createElement('li')
-                 newElements.innerText = selectedNyInnerText
-                 if (arrayList.length < 5) {
-                    arrayList.push(container)
-                    disableBtn.disabled = true;
-                    disableBtn.style.backgroundColor = "gray"
-                    container.appendChild(newElements)
-                }
-                else{
-                    alert ('You can chose only five')
-                }
-                 
+document.getElementById('selected2').addEventListener('click',function(){
+    getYourBest('neymar', 'selected2')
 })
-document.getElementById('selected3').addEventListener('click', function(){
-
-            const selectMbBtn = document.getElementById('mb');
-            const selectedMbInnerText = selectMbBtn.innerText
-
-            const disableBtn = document.getElementById('selected3')
-            const container=  document.getElementById("player-Container");
-                 newElements = document.createElement('li')
-                 newElements.innerText = selectedMbInnerText
-                 if (arrayList.length < 5) {
-                    arrayList.push(container)
-                    disableBtn.disabled = true;
-                    disableBtn.style.backgroundColor = "gray"
-                    container.appendChild(newElements)
-                }
-                else{
-                    alert ('You can chose only five')
-                }
-                 
+document.getElementById('selected3').addEventListener('click',function(){
+    getYourBest('mb', 'selected3')
 })
-document.getElementById('selected4').addEventListener('click', function(){
-
-     const selectViBtn = document.getElementById('victor');
-     const selectedViInnerText = selectViBtn.innerText
-
-     const disableBtn = document.getElementById('selected4')
-     const container=  document.getElementById("player-Container");
-                 newElements = document.createElement('li')
-                 newElements.innerText = selectedViInnerText
-                 if (arrayList.length < 5) {
-                    arrayList.push(container)
-                    disableBtn.disabled = true;
-                    disableBtn.style.backgroundColor = "gray"
-                    container.appendChild(newElements)
-                }
-                else{
-                    alert ('You can chose only five')
-                }
+document.getElementById('selected4').addEventListener('click',function(){
+    getYourBest('victor', 'selected4')
 })
-document.getElementById('selected5').addEventListener('click', function(){
-
-        const selectRamosBtn = document.getElementById('ramos');
-        const selectedRamosInnerText = selectRamosBtn.innerText
-        const disableBtn = document.getElementById('selected5')
-        const container=  document.getElementById("player-Container");
-                 newElements = document.createElement('li')
-                 newElements.innerText = selectedRamosInnerText
-                 if (arrayList.length < 5) {
-                    arrayList.push(container)
-                    disableBtn.disabled = true;
-                    disableBtn.style.backgroundColor = "gray"
-                    container.appendChild(newElements)
-                }
-                else{
-                    alert ('You can chose only five')
-                }
+document.getElementById('selected5').addEventListener('click',function(){
+    getYourBest('ramos', 'selected5')
 })
-document.getElementById('selected6').addEventListener('click', function(){
-
-    const selectRenaBtn = document.getElementById('renato');
-    const selectedRenaInnerText = selectRenaBtn.innerText
-
-    const disableBtn = document.getElementById('selected6')
-    const container=  document.getElementById("player-Container");
-                 newElements = document.createElement('li')
-                 newElements.innerText = selectedRenaInnerText
-                if (arrayList.length < 5) {
-                    container.appendChild(newElements)
-                    arrayList.push(container)
-                    disableBtn.disabled = true;
-                    disableBtn.style.backgroundColor = "gray"
-                  }
-                else{
-                    alert ('You can chose only five')
-                }
+document.getElementById('selected6').addEventListener('click',function(){
+    getYourBest('renato', 'selected6')
 })
 
 function getYourCalculation() {
@@ -156,8 +76,8 @@ function getYourGrandTotal(){
     console.log(totalCalculation)
     if (isNaN(managerValueString) || isNaN(coachValueString)) {
         alert ('please enter a valid input and Full-fill the input')
-        manager.value = ''
-        coach.value = ''
+        // manager.value = ''
+        // coach.value = ''
     }
     // if (isNaN(managerValueString) ) {
     //     alert ('please enter a valid input and Full-fill the input')
@@ -190,20 +110,11 @@ if (isNaN(finalTotal.innerText)) {
     return finalTotal.innerText = 000;
 }
 }
-
-
-
 document .getElementById('calculate-btn').addEventListener('click',function(){
-
-    getYourCalculation()
-
+     getYourCalculation()
 })
 document.getElementById('total').addEventListener('click', function(){
-    
-    getYourGrandTotal()
-    getYourGrandCalculationTotal()
-
+     getYourGrandTotal()
+     getYourGrandCalculationTotal()
 })
-
-
 
